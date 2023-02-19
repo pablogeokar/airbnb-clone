09a1520f3a6902a7e9c6a63da4026a091919eb1e
Pablo George <pablogeokar@hotmail.com>
2023-02-19 20:45:19 -0300

    starting component creating Banner

--- airbnb/components/Header.tsx
+++ airbnb/components/Header.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import {
+  MagnifyingGlassIcon,
+  GlobeAltIcon,
+  Bars3Icon,
+  UserCircleIcon,
+  UsersIcon,
+} from "@heroicons/react/24/solid";
 
 export function Header() {
   return (
@@ -18,7 +24,7 @@ export function Header() {
       {/** Middle - Search */}
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
-          className="flex-grow pl-5 bg-transparent outline-none"
+          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
           type="text"
           placeholder="Start your search"
         />
@@ -26,7 +32,14 @@ export function Header() {
       </div>
 
       {/** Right */}
-      <div></div>
+      <div className="flex items-center space-x-4 justify-end text-gray-500">
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full ">
+          <Bars3Icon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
     </header>
   );
 }
--- airbnb/pages/index.tsx
+++ airbnb/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { Banner } from "../components/Banner";
 import { Header } from "./../components/Header";
 
 const Home: NextPage = () => {
@@ -10,7 +11,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      {/** Banner */}
+      <Banner />
     </div>
   );
 };
