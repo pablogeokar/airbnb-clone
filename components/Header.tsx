8bed96b3fc5b7cf0701d4e19b16d7b4f022a951c
Pablo George <pablogeokar@hotmail.com>
2023-02-26 11:36:14 -0300

    DAY ONE

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,11 @@ import {
   Bars3Icon,
   UserCircleIcon,
 } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 export function Header() {
+  const [searchInput, setSearchInput] = useState("");
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/** left */}
@@ -22,9 +25,11 @@ export function Header() {
       {/** Middle - Search */}
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
+          value={searchInput}
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
           type="text"
           placeholder="Start your search"
+          onChange={(e) => setSearchInput(e.target.value)}
         />
         <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
